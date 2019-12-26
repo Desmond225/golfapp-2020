c8fd5697709325f74cf74dbea61ec14b258a08e6
terefs <d.van.zurk@gmail.com>
2019-12-26 15:14:04 +0100

    added register & signin components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
 import React, { Component } from 'react';
 import './App.css';
-import Navigation from './components/navigation/navigation'
-import RoundList from './components/roundlist/roundlist'
-import Statistics from './components/statistics/statistics'
+import Navigation from './components/navigation/navigation';
+import RoundList from './components/roundlist/roundlist';
+// import Statistics from './components/statistics/statistics';
+// import Menu from './components/menu/menu';
+import Signin from './components/signin/signin';
+import Register from './components/register/register';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      route: 'home'
+      route: 'signin',
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        submitted_rounds: 0,
+        joined: ''
+      }
     }
   }
 
+  loadUser = (data) => {
+    this.setState({user: {
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      submitted_rounds: data.submitted_rounds,
+      joined: data.joined
+    }})
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
   onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState({isSignedIn: false})
+    }
+    else if(route === 'home') {
+      this.setState({isSignedIn: true})
+    }
     this.setState({route: route});
   }
 
   render() {
-    const {route} = this.state;
+    const { isSignedIn, route } = this.state;
     return (
       <div className="App">
         <Navigation 
@@ -25,16 +56,13 @@ class App extends Component {
         />
         { route === 'home' 
           ? 
-          <div className={{display: 'flex', flexDirection: 'column'}}>
-            <button className='f4 w-100 link dim ph4 pv2 dib white bg-blue tl'>Play golf (coming soon)</button>
-            <button className='f4 w-100 link dim ph4 pv2 dib white bg-blue tl'>Book teetimes (coming soon)</button>
-            <button className='f4 w-100 link dim ph4 pv2 dib white bg-blue tl'>Learn more (coming soon)</button>
-          </div>
+            // <Menu />
+            <RoundList />
           : (
-            route === 'roundlist'
-            ? <RoundList />
-            : <Statistics /> 
-          )          
+            route === 'signin'
+            ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+            : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+          )        
         }
 
       </div>
