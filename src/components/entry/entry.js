5dee7a71ef1b8b3f0fec8151468eddccd505faa9
terefs <d.van.zurk@gmail.com>
2020-01-05 21:57:27 +0100

    Rendering courses & tee colors as options

--- src/components/entry/entry.js
+++ src/components/entry/entry.js
@@ -1,34 +1,79 @@
 import React from 'react';
 
-const Entry = () => {
+class Entry extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            courses: '',
+            teeColors: ''
+        }
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:3005/courses')
+        .then(response => response.json())
+        .then(courses => {
+            console.log('courses ', courses);
+            this.setState({courses: courses})
+            // this.setState({courses: courses[0].name})
+            // this.setState({tracks: courses[0].track})
+        })
+        .catch(console.log('error fetching courses'));
+
+        fetch('http://localhost:3005/tee-colors')
+        .then(response => response.json())
+        .then(teeColors => {
+            this.setState({teeColors: teeColors})
+        })
+    }
+
+    render() {
+        let arrayOfCourses = Array.from(this.state.courses);
+        let arrayOfTeeColors = Array.from(this.state.teeColors);
+
+        let courseOptions = arrayOfCourses.map((course) =>
+        <option 
+            key={course.id}
+            value={course.id}
+        >
+            {course.name, course.track}
+        </option>
+    );
+
+    let teeColorOptions = arrayOfTeeColors.map((tee) =>
+    <option 
+        key={tee.id}
+        value={tee.id}
+    >
+        {tee.tee_color}
+    </option>
+);
     return (
         <div>
         <div className="mt3 w-40 center">
-            <label className="db fw6 lh-copy f6" htmlFor="round-score">Course</label>
-            <select>
-                <option value="volvo">Volvo</option>
-                <option value="saab">Saab</option>
-                <option value="mercedes">Mercedes</option>
-                <option value="audi">Audi</option>
-            </select>
+            <label className="db fw6 lh-copy f6" htmlFor="round-score">Front Course</label>
+            <select name="customSearch" className="custom-search-select">
+                <option>Select Front Course</option>
+                {courseOptions}
+           </select>
         </div>
         <div className="mt3 w-40 center">
-            <label className="db fw6 lh-copy f6" htmlFor="round-score">Select track</label>
-            <select>
-                <option value="volvo">front 9</option>
-                <option value="saab">back 9</option>
-            </select>
+            <label className="db fw6 lh-copy f6" htmlFor="round-score">Back Course</label>
+            <select name="customSearch" className="custom-search-select">
+                <option>No back course</option>
+                {courseOptions}
+           </select>
         </div>
         <div className="mt3 w-40 center">
-            <label className="db fw6 lh-copy f6" htmlFor="round-score">Select tee color</label>
-            <select>
-                <option value="volvo">White</option>
-                <option value="saab">Yellow</option>
-            </select>
+            <label className="db fw6 lh-copy f6" htmlFor="round-score">Tee Color</label>
+            <select name="customSearch" className="custom-search-select">
+                {teeColorOptions}
+           </select>
         </div>
             {/* <button onClick={this.onSubmitRound}>Submit round</button> */}
         </div>
     )
+    }
 }
 
 export default Entry
