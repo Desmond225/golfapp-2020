1dadc5e4279be0321623f00977dfa65f9c534e4f
terefs <d.van.zurk@gmail.com>
2020-01-09 15:59:51 +0100

    Rendering first rounds data from the db

--- src/components/round/round.js
+++ src/components/round/round.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
-const Round = ({ score, course, id }) => {
+const Round = ({ id, score, course, playing_date, score_to_par, fairways_in_regulation, greens_in_regulation, putts_per_gir, putts }) => {
+    let rawDate = new Date(playing_date);
+    let formattedDate = rawDate.getDate() + "-" + parseInt(rawDate.getMonth()+1) + "-" + rawDate.getFullYear();
+
     return (
         <div
             className='pv3 ph3' 
@@ -13,18 +16,20 @@ const Round = ({ score, course, id }) => {
                 style={{alignSelf: 'center'}}
                 >{score}</div>
                 <div className='tl'>
-                    <div className='b'>{id}</div>
+                    <div className='b'>{formattedDate}</div>
                     <div>{course}</div>
                 </div>
             </div>
             <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
-                <div>Score to par</div>
-                <div>FIR</div>
-                <div>GIR</div>
-                <div>Putts per gir</div>
+                <div>+{score_to_par}</div>
+                <div>FIR: {fairways_in_regulation}/13</div>
+                <div>GIR: {greens_in_regulation}/18</div>
+                <div>Putts per gir: {putts_per_gir}</div>
+                <div>Putts: {putts}</div>
             </div>
         </div>
     )
+
 }
 
 export default Round
--- src/components/roundlist/roundList.js
+++ src/components/roundlist/roundList.js
@@ -12,8 +12,14 @@ const RoundList = ({rounds}) => {
                 <Round 
                 key={i}
                 id={rounds[i].id}
+                playing_date={rounds[i].playing_date}
                 course={rounds[i].course}
                 score={rounds[i].score}
+                score_to_par={rounds[i].score_to_par}
+                fairways_in_regulation={rounds[i].fairways_in_regulation}
+                greens_in_regulation={rounds[i].greens_in_regulation}
+                putts={rounds[i].putts}
+                putts_per_gir={rounds[i].putts_per_gir}
                 />
               );
             })
