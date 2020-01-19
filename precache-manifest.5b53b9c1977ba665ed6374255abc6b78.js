3551da252804629dabd11bfd26218592dff6af17
terefs <d.van.zurk@gmail.com>
2020-01-19 15:06:20 +0100

    Updates

---
+++ precache-manifest.5b53b9c1977ba665ed6374255abc6b78.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7130ae58ddb65859482e30ed78bda12",
+    "url": "/golfapp-2020/index.html"
+  },
+  {
+    "revision": "a31742e40a857c37d8d9",
+    "url": "/golfapp-2020/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "980c83e7144eea6f3f37",
+    "url": "/golfapp-2020/static/css/main.55c68f93.chunk.css"
+  },
+  {
+    "revision": "a31742e40a857c37d8d9",
+    "url": "/golfapp-2020/static/js/2.22419d15.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/golfapp-2020/static/js/2.22419d15.chunk.js.LICENSE"
+  },
+  {
+    "revision": "980c83e7144eea6f3f37",
+    "url": "/golfapp-2020/static/js/main.1cc06044.chunk.js"
+  },
+  {
+    "revision": "ec845e7bc53568bf075b",
+    "url": "/golfapp-2020/static/js/runtime-main.2718fde9.js"
+  },
+  {
+    "revision": "f2846a8a0350e6dabd57fd86632e6613",
+    "url": "/golfapp-2020/static/media/logo.f2846a8a.png"
+  }
+]);
